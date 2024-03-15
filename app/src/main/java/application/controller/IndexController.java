1e4f9b6d8079392856376ca4b66cbf24ba5af502
IS_Frisker <126285571+BernardoBomfim@users.noreply.github.com>
2024-03-15 19:38:43 +0000

    Add false requiered in name action

--- app/src/main/java/application/controller/IndexController.java
+++ app/src/main/java/application/controller/IndexController.java
@@ -2,14 +2,15 @@ package application.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 
 @Controller
 public class IndexController {
     @RequestMapping("/")
-    public String index(Model ui
-            @RequestParam("nome") String n){
-        ui.addAttribute("msg", "Olá" + n + "!!!");
+    public String index(Model ui,
+            @RequestParam(name="nome", required = false) String n){
+        ui.addAttribute("msg", "Olá " + n + "!!!");
         return "/WEB-INF/home.jsp";
     }
 
