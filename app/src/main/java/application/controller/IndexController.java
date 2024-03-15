8799a8134f3be4c46b59f431ac26561e58bc6ada
IS_Frisker <126285571+BernardoBomfim@users.noreply.github.com>
2024-03-15 19:29:50 +0000

    Sla

---
+++ app/src/main/java/application/controller/IndexController.java
@@ -0,0 +1,16 @@
+package application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index(Model ui
+            @RequestParam("nome") String n){
+        ui.addAttribute("msg", "Olá Spring!!!");
+        return "/WEB-INF/home.jsp";
+    }
+
+}
---
+++ app/src/main/webapp/home.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html lang="pt-bt">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Exemplo JSP</title>
+    </head>
+    <body>
+        <h1>Saldação: ${msg}</h1>
+    </body>
+</html>
